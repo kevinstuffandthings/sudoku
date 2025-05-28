ac7b8b9e99cf8d5cee5420a6ecd38a76952288fb
Kevin McDonald <kevinstuffandthings@gmail.com>
2025-05-28 18:20:52 -0400

    adding some tests -- one failure!

---
+++ spec/puzzle_spec.rb
@@ -0,0 +1,15 @@
+module Sudoku
+  describe Puzzle do
+    Dir["spec/puzzles/*.txt"].each do |filename|
+      context filename do
+        let(:puzzle) { Sudoku::Puzzle.seed(filename) }
+        let(:solver) { Sudoku::Puzzle::Solver.new(puzzle) }
+
+        it "gets solved" do
+          solver.solve
+          expect(puzzle).to be_solved
+        end
+      end
+    end
+  end
+end
