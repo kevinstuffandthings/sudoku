2f474899e924dda4f5649e40e8b13994c741256f
Kevin McDonald <kevinstuffandthings@gmail.com>
2025-05-28 19:41:43 -0400

    fixed up the tests

--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -1,6 +1,6 @@
 module Sudoku
   describe Puzzle do
-    Dir["spec/puzzles/*.txt"].each do |filename|
+    Dir["spec/puzzles/*.sudoku"].each do |filename|
       context filename do
         let(:puzzle) { Sudoku::Puzzle.seed(filename) }
         let(:solver) { Sudoku::Puzzle::Solver.new(puzzle) }
